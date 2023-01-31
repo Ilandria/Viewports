f3db75f9c43fcc5e19051fd00bc3fa4c450d0ede
Charlotte C. Brown <charlotte.clover.brown@gmail.com>
2023-01-31 17:54:53 -0500

    Basic raycasting while hovering over viewport.

--- Assets/Source/OrthoViewport/ViewportRaycaster.cs
+++ Assets/Source/OrthoViewport/ViewportRaycaster.cs
@@ -1,22 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 
 [RequireComponent(typeof(Camera))]
 public class ViewportRaycaster : MonoBehaviour
 {
+	private Camera camera = null;
+
+	[SuppressMessage("CodeQuality", "IDE0051")]
+	private void Start()
+	{
+		camera = GetComponent<Camera>();
+	}
+
 	public void StartTracking()
 	{
-
 	}
 
 	public void Track(Vector3 position)
 	{
-
+		// Todo: fix the math here so it lines up properly.
+		Ray ray = camera.ViewportPointToRay(position);
+		Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100.0f);
 	}
 
 	public void StopTracking()
 	{
-
 	}
 }

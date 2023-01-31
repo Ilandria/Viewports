c08b81e0a803251f73c3a92f8d95280eb6117d81
Charlotte C. Brown <charlotte.clover.brown@gmail.com>
2023-01-31 16:19:45 -0500

    Initial project setup

---
+++ Assets/Source/OrthoViewport/ViewportPanelScaler.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ViewportPanelScaler : MonoBehaviour
+{
+	[SerializeField]
+	private float PanelWidth = 100.0f;
+
+	private int numChildren;
+
+	private void Awake()
+	{
+
+	}
+}

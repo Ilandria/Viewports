7b4e2d0ea3271dd7293377dd9e8c231c6a8799a0
Charlotte C. Brown <charlotte.clover.brown@gmail.com>
2023-01-31 16:57:32 -0500

    Scaler for the viewport panel because I was tired of manually adjusting it.

--- Assets/Source/OrthoViewport/ViewportPanelScaler.cs
+++ Assets/Source/OrthoViewport/ViewportPanelScaler.cs
@@ -1,17 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 
 [ExecuteInEditMode]
+[RequireComponent(typeof(RectTransform))]
 public class ViewportPanelScaler : MonoBehaviour
 {
 	[SerializeField]
-	private float PanelWidth = 100.0f;
+	private float panelWidth = 100.0f;
 
-	private int numChildren;
+	private RectTransform rectTransform = null;
 
-	private void Awake()
+	// Hacky way around the warning spam from updating values in OnValidate. Other solution is to make a custom inspector, but that's overkill.
+	[SuppressMessage("CodeQuality", "IDE0051")]
+	private void OnValidate() => Invoke("Resize", 0.0f);
+
+	[SuppressMessage("CodeQuality", "IDE0051")]
+	private void Awake() => Resize();
+
+	private void Resize()
 	{
+		if (rectTransform == null)
+		{
+			rectTransform = GetComponent<RectTransform>();
+		}
 
+		rectTransform.sizeDelta = new Vector2(panelWidth, panelWidth * rectTransform.childCount);
 	}
 }

17f11bddbc983e17c8e3f6ef9400cec9ca08516c
Charlotte C. Brown <charlotte.clover.brown@gmail.com>
2023-01-31 23:51:26 -0500

    Grab & move implemented.
    Also set all post-processing effects to some variant of 42.

---
+++ Assets/Source/Grabbable.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Grabbable : MonoBehaviour
+{
+    private bool isGrabbed = false;
+    private Vector3 grabOffset = Vector3.zero;
+
+    public void Hover(Transform hoveredTransform, Vector3 worldPosition)
+    {
+        if (hoveredTransform != transform)
+        {
+            return;
+        }
+
+        if (Input.GetMouseButtonDown((int)MouseButton.LeftMouse))
+        {
+            isGrabbed = true;
+            grabOffset = transform.position - worldPosition;
+        }
+    }
+
+    public void Move(Vector3 worldPosition)
+    {
+        if (isGrabbed)
+        {
+            transform.position = worldPosition + grabOffset;
+        }
+    }
+
+    public void Release()
+    {
+        isGrabbed = false;
+    }
+
+    public void Update()
+    {
+        if (isGrabbed && Input.GetMouseButtonUp((int)MouseButton.LeftMouse))
+        {
+            isGrabbed = false;
+        }
+    }
+}
--- Assets/Source/OrthoViewport/ViewportRaycaster.cs
+++ Assets/Source/OrthoViewport/ViewportRaycaster.cs
@@ -2,10 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Camera))]
 public class ViewportRaycaster : MonoBehaviour
 {
+	[SerializeField]
+	private LayerMask mask;
+
+	[SerializeField]
+	[SuppressMessage("Style", "IDE0044")]
+	private UnityEvent<Transform, Vector3> OnRaycastHit = new UnityEvent<Transform, Vector3>();
+
+	[SerializeField]
+	[SuppressMessage("Style", "IDE0044")]
+	private UnityEvent<Vector3> OnRaycast = new UnityEvent<Vector3>();
+
+	[SerializeField]
+	[SuppressMessage("Style", "IDE0044")]
+	private UnityEvent OnViewportExit = new UnityEvent();
+
 	private Camera viewportCamera = null;
 
 	[SuppressMessage("CodeQuality", "IDE0051")]
@@ -16,16 +32,24 @@ public class ViewportRaycaster : MonoBehaviour
 
 	public void StartTracking()
 	{
+		// Not currently used.
 	}
 
 	public void Track(Vector3 position)
 	{
 		// Todo: fix the math here so it lines up properly.
 		Ray ray = viewportCamera.ViewportPointToRay(position);
-		Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100.0f);
+
+		OnRaycast?.Invoke(ray.origin);
+
+		if(Physics.Raycast(ray, out RaycastHit hitInfo, viewportCamera.farClipPlane - viewportCamera.nearClipPlane, mask.value))
+		{
+			OnRaycastHit?.Invoke(hitInfo.transform, ray.origin);
+		}
 	}
 
 	public void StopTracking()
 	{
+		OnViewportExit?.Invoke();
 	}
 }

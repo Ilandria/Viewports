b0d27f2e8b5cadfc55c41b21f4f3c1ab70eb0ab1
Charlotte C. Brown <charlotte.clover.brown@gmail.com>
2023-02-02 16:38:50 -0500

    Updated standalone build.

--- Assets/Source/VideoClipLoader.cs
+++ Assets/Source/VideoClipLoader.cs
@@ -4,16 +4,12 @@ using UnityEngine.Video;
 [RequireComponent(typeof(VideoPlayer))]
 public class VideoClipLoader : MonoBehaviour
 {
-	// Originally hosted here: https://dl.dropboxusercontent.com/s/8uvbpwnrck1qhqq/Wrongwarp.mp4
-	[SerializeField]
-	private string videoUrl = string.Empty;
-
+	// Originally hosted here: https://github.com/Ilandria/Viewports/raw/main/Assets/StreamingAssets/Wrongwarp.mp4
 	private VideoPlayer videoPlayer = null;
 
 	public void Start()
 	{
 		videoPlayer = GetComponent<VideoPlayer>();
-		videoPlayer.url = videoUrl;
 		videoPlayer.Prepare();
 	}
 }
